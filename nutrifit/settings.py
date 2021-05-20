2493ca0a4b9bdb064bc8f215402797a8070fa810
dahs <hendra.dhonni.888@gmail.com>
2021-05-20 13:56:31 +0700

    add STATICFILES_DIRS

--- nutrifit/settings.py
+++ nutrifit/settings.py
@@ -146,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 PROJECT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
