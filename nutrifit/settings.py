c4efaa696d66e9a8c482005d27280e8dd558d597
dahs <hendra.dhonni.888@gmail.com>
2021-05-24 14:33:39 +0700

    add credentials

--- nutrifit/settings.py
+++ nutrifit/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -105,16 +105,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': "DB_NUTRIFIT",
-#         'USER': 'postgres',
-#         'PASSWORD': 'Caw123qwe455@',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
