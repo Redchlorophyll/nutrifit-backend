7b679e1291ce4b79f5fef00369a63c28c8fec108
dahs <hendra.dhonni.888@gmail.com>
2021-05-28 06:43:54 +0700

    add main.py and sme change in setting

--- nutrifit/settings.py
+++ nutrifit/settings.py
@@ -98,24 +98,26 @@ WSGI_APPLICATION = 'nutrifit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv("GAE_APPLICATION", None):
+    # postgres with cloud SQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USERNAME'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            # 'HOST': os.environ.get('DB_HOST'),
+            'HOST': "/cloudsql/x-entropy-313703:asia-southeast2:nutrifit",
+            'PORT': '5432'
+        }
     }
-}
-
-# postgres with cloud SQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USERNAME'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': '5432'
-#     }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
